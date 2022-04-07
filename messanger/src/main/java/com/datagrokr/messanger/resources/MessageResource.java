fa5d69e163b29e1df9dcfbecbd43b43f61583307
sammed-dev <sammed.sankonatti@datagrokr.com>
2022-04-07 15:06:49 +0530

    all the REST endpoints are tested

--- messanger/src/main/java/com/datagrokr/messanger/resources/MessageResource.java
+++ messanger/src/main/java/com/datagrokr/messanger/resources/MessageResource.java
@@ -4,7 +4,11 @@ import java.util.List;
 import com.datagrokr.messanger.model.Message;
 import com.datagrokr.messanger.service.MessageService;
 
+import jakarta.ws.rs.Consumes;
+import jakarta.ws.rs.DELETE;
 import jakarta.ws.rs.GET;
+import jakarta.ws.rs.POST;
+import jakarta.ws.rs.PUT;
 import jakarta.ws.rs.Path;
 import jakarta.ws.rs.PathParam;
 import jakarta.ws.rs.Produces;
@@ -17,7 +21,7 @@ public class MessageResource {
 	
 
 	@GET
-	@Produces(MediaType.APPLICATION_XML)
+	@Produces(MediaType.APPLICATION_JSON)
 	public List<Message> getMessages() {
 		return messageService.getAllMessages();
 	}
@@ -27,13 +31,41 @@ public class MessageResource {
 	public String welcomeMessage() {
 		return "Hello there";
 	}
-	
-	
+//	
+//	
 	@GET
-	@Produces(MediaType.APPLICATION_XML)
+	@Produces(MediaType.APPLICATION_JSON)
 	@Path("/{messageid}")
 	public Message getMessage(@PathParam("messageid") Long id) {
 		return messageService.getMessage(id);
 	}
+	
+	
+	@POST
+	@Consumes(MediaType.APPLICATION_JSON)
+	@Produces(MediaType.APPLICATION_JSON)
+	@Path("/addmessage")
+	public Message postMessage(Message message) {
+		return messageService.addMessage(message);
+	}
+	
+	
+	@PUT
+	@Path("/update")
+	@Consumes(MediaType.APPLICATION_JSON)
+	@Produces(MediaType.APPLICATION_JSON)
+	public Message updateMessage(@PathParam("id") Long id, Message message ) {
+		message.setId(id);
+		return messageService.updateMessage(message);
+	}
+	
+	
+	@DELETE
+	@Path("/delete")
+	@Produces(MediaType.APPLICATION_JSON)
+	public void deleteMessage(@PathParam("id") Long id) {
+		messageService.deleteMessage(id);
+	}
+	
 	
 }
